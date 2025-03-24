a8b724691a4461afc03ae71b94e6cc8abce16d6a
cheapskatecoder <cheapskatecoder@gmail.com>
2025-03-25 02:00:26 +0530

    Update the installation file

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# I like the fact that you opened the file to see what the fuck this script might do to your system. 
+# I like the fact that you opened the file to see what the fuck this script might do to your system.
 # Good Job! Stay secure my guy.
 
-# installing vim.
+# Ensure the .vim directory exists
+mkdir -p ~/.vim/bundle
+
+# Installing Vundle -- A vim package manager.
 echo "Installing Vundle -- A vim package manager."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-
-# copying contents of my .vimrc to your .vimrc
-echo .vimrc >> ~/.vimrc
+# Copying contents of my .vimrc to your .vimrc
+echo "Copying .vimrc to your home directory."
+cp ./.vimrc ~/.vimrc
 
 # Downloading all the plugins to make your life easier
-vim +PlugInstall +qall
+echo "Installing Vim plugins using Vundle."
+vim +PluginInstall +qall
+
+echo "Installation complete!"
+
